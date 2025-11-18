b8c6d2b8ed4631f402928934b75b70be4bd4328b
Dmitriy Nazimov <ArhDemetriy@GMail.com>
2025-11-18 17:54:18 +0800

    init button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return null;
-}
+import { Button, Modal } from 'antd';
 
-export default App;
+import { OrderCreateModal } from '@/OrderCreateModal';
+import { PlusOutlined } from '@ant-design/icons';
+
+export function App() {
+  const [modal, contextHolder] = Modal.useModal();
+
+  const showModal = () => {
+    modal.info({
+      title: 'Мобильная форма создания заказа',
+      content: <OrderCreateModal />,
+    });
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      {contextHolder}
+      <Button type="primary" icon={<PlusOutlined />} onClick={showModal}>
+        Открыть модалку
+      </Button>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+import { App as AntdApp } from 'antd';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import { App } from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AntdApp>
+      <App />
+    </AntdApp>
   </StrictMode>
 );
